3ae389267edbc8a4727c111705946726d639fac7
Marc Zafra Carmona <marczc06@gmail.com>
2025-06-22 00:45:54 +0200

    Update freddy.js

--- FNAF/api/freddy.js
+++ FNAF/api/freddy.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Only POST allowed' });
 
-  const apiKey = process.env.OPENAI_API_KEY_;
+  const apiKey = process.env.OPENAI_API_KEY;
   const { question } = req.body;
 
   const systemPrompt = `
